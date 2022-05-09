01707181b4a8f483a29616bc4420b486ab768037
Priya Aggarwal <priya.aggarwal0125@gmail.com>
2022-05-09 13:00:59 -0400

    Implement thank you state in rating component

--- interactive-rating-component/app.js
+++ interactive-rating-component/app.js
@@ -1,6 +1,22 @@
 (function() {
 
     const thankComponent = document.querySelector("#thank-state");
-    thankComponent.style.display = "none";
+    const ratingComponent = document.querySelector("#rating-state");
+    const ratingForm = document.querySelector("#rating-form");
+
+    ratingForm.addEventListener("submit", function(event) {
+        // Prevent browser from refreshing the page
+        event.preventDefault();
+        const selector = `input[name="rating"]:checked`
+        const checkedInput = document.querySelector(selector);
+
+        // There must be a checked radio input to show thank you state
+        if (checkedInput !== null) {
+            const userRating = document.querySelector("#rating-chose");
+            userRating.textContent = checkedInput.getAttribute("value");
+            ratingComponent.classList.add("hide");
+            thankComponent.classList.remove("hide");
+        }
+    });
 
 })();
