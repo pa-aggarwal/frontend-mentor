315289813ca50f332687c3cf278c1aa6ab5478e1
Priya Aggarwal <priya.aggarwal0125@gmail.com>
2022-05-13 18:08:32 -0400

    Add share button mobile view for article component

--- article-preview-component/app.js
+++ article-preview-component/app.js
@@ -1,9 +1,50 @@
+(function() {
 
-const shareButton = document.querySelector("#share-btn");
-const shareButtonBox = document.querySelector("#share-btn-box");
-const sharePopup = document.querySelector("#share-popup");
+    const shareButton = document.querySelector("#share-btn");
+    const shareButtonBox = document.querySelector("#share-btn-box");
+    const sharePopup = document.querySelector("#share-popup");
+    const footer = document.querySelector("#component-footer");
+    const footerInfo = document.querySelector("#component-footer-info");
+    let isShowingShare = false;
 
-shareButton.addEventListener("click", function() {
-    sharePopup.classList.toggle("show-popup");
-    shareButton.classList.toggle("active-share-btn");
-});
+    function transferChildren(parentFrom, parentTo) {
+        while (parentFrom.firstChild !== null) {
+            let child = parentFrom.removeChild(parentFrom.firstChild);
+            parentTo.appendChild(child);
+        }
+    }
+
+    function showShareOnMobile() {
+        let shareContent;
+        sharePopup.classList.remove("show-popup-desktop");
+        sharePopup.classList.toggle("show-popup-mobile");
+        footer.classList.toggle("component-footer");
+        footerInfo.classList.toggle("no-display");
+
+        if (!isShowingShare) {
+            shareContent = document.createElement("div");
+            shareContent.classList.add("share-left");
+            transferChildren(sharePopup, shareContent);
+            shareButtonBox.removeChild(shareButton);
+            sharePopup.appendChild(shareContent);
+            sharePopup.appendChild(shareButton);
+            isShowingShare = true;
+        } else {
+            shareContent = sharePopup.removeChild(sharePopup.firstChild);
+            sharePopup.removeChild(shareButton);
+            shareButtonBox.appendChild(shareButton);
+            transferChildren(shareContent, sharePopup);
+            isShowingShare = false;
+        }
+    }
+
+    shareButton.addEventListener("click", function() {
+        this.classList.toggle("active-share-btn");
+        if (window.innerWidth > 650) {
+            sharePopup.classList.toggle("show-popup-desktop");
+        } else {
+            showShareOnMobile();
+        }
+    });
+
+})();
